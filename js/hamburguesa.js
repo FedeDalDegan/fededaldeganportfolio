ec726b58c16154f8d585051aa27014ce639a1c56
FedeDalDegan <federiconahueldaldegan@hotmail.com>
2023-10-31 20:37:44 -0300

    Arreglos en mediaquerys y modo obscuro

--- js/hamburguesa.js
+++ js/hamburguesa.js
@@ -6,30 +6,29 @@ const cerrarBtn = document.getElementById("cerrar");
 const nav = document.getElementById("nav");
 
 // Agregar eventos click para mostrar y ocultar la navegación
-abrirBtn.addEventListener("click", function() {
-  nav.classList.add("visible");
+abrirBtn.addEventListener("click", function () {
+    nav.classList.add("visible");
 });
 
-cerrarBtn.addEventListener("click", function() {
-  nav.classList.remove("visible");
+cerrarBtn.addEventListener("click", function () {
+    nav.classList.remove("visible");
 });
 
 // Modo obscuro
-
 const toggleModeButton = document.getElementById("toggle-mode");
 const body = document.body;
 
-window.addEventListener("load", function() {
-  const isDarkModeSaved = localStorage.getItem("darkModeEnabled");
-  if (isDarkModeSaved === "true") {
-    body.classList.add("dark-mode");
-  }
+window.addEventListener("load", function () {
+    const isDarkModeSaved = localStorage.getItem("darkModeEnabled");
+    if (isDarkModeSaved === "true") {
+        body.classList.add("dark-mode");
+    }
 });
 
-toggleModeButton.addEventListener("click", function() {
-  body.classList.toggle("dark-mode");
-  const isDarkMode = body.classList.contains("dark-mode");
-  localStorage.setItem("darkModeEnabled", isDarkMode); // Guardar el estado en el almacenamiento local
+toggleModeButton.addEventListener("click", function () {
+    body.classList.toggle("dark-mode");
+    const isDarkMode = body.classList.contains("dark-mode");
+    localStorage.setItem("darkModeEnabled", isDarkMode); // Guardar el estado en el almacenamiento local
 });
 
 
